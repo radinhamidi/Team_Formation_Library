151b6a010276a690559c94fbcd34a4748eb58069
aabidmitha10 <aabid.mitha@ontariotechu.net>
2020-11-22 20:48:07 -0500

    Basic update to main and dataaccesslayer files

--- teamFormationLibrary/__main__.py
+++ teamFormationLibrary/__main__.py
@@ -1,16 +1,13 @@
-# This is a sample Python script.
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from teamFormationLibrary.data_access_layer import DataAccessLayer
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def main_team_formation():
+    database_name = input("Please enter the name of your database:")
+    database_path = input("Please paste the path of your database:")
+    DAL = DataAccessLayer(database_name, database_path)
+    print(DAL.get_database_name())
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main_team_formation()
--- teamFormationLibrary/data_access_layer.py
+++ teamFormationLibrary/data_access_layer.py
@@ -1,3 +1,13 @@
+class DataAccessLayer:
+    def __init__(self, database_name, database_path):
+        self.database_name = database_name
+        self.databasePath = database_path
 
-def printHelloWorld():
-    print("hello world!")
+    def get_database_name(self):
+        return self.database_name
+
+    def get_database_path(self):
+        return self.databasePath
+
+    def pre_process_data(self):
+        return 1
